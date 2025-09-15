544548dd4f653755601bbb8d96e6e0b04189d343
Layan Alsayed <laymix25@gmail.com>
2025-09-15 12:53:40 +0300

    Edit config folder: App.php and Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => '',
         'password'     => '',
-        'database'     => 'finaldb',
+        'database'     => '', //Keep it empty
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
